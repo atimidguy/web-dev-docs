3f620e13b64cab920c2d0a962e154a32b77cb2ed
atimidguy <dumingcong0119@gmail.com>
2020-01-12 22:58:41 +0800

    try add pwa -2

--- .vuepress/config/headConf.js
+++ .vuepress/config/headConf.js
@@ -1,6 +1,5 @@
 module.exports = [
   ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
-  ['link', { rel: 'icon', href: '/assets/img/logo.png' }],
   ['link', { rel: 'manifest', href: '/manifest.json' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
